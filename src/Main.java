fba486523a9844a814bc362ae97dab46d0b613c1
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-06-03 11:12:57 +0200

    working server

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("STATKI TM");
+
+
+
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+
+            while (true) {
+                new Echoer(serverSocket.accept()).start();
+                System.out.println("cos");
+
+            }
+
+
+
+        } catch (IOException e) {
+            System.out.println("Server error");
+        }
     }
 }
